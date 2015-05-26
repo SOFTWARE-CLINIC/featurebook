d1aabc04baa379ab7aac2fb2c922a11c8dacd544
Daniel Pacak <pacak.daniel@gmail.com>
2015-05-26 15:45:33 +0200

    add unit tests for directory walker

---
+++ test/directory-walker-spec.js
@@ -0,0 +1,25 @@
+var walker = require('../lib/directory-walker'),
+  assert = require('assert');
+
+describe('directory-walker', function () {
+
+  describe('#walk', function () {
+    it('should return directory tree', function () {
+      var tree = walker.walk('test/resources', '.');
+
+      assertNodeEqual(tree, '.', 'folder', '.');
+
+      assert.equal(tree.items.length, 2);
+
+      assertNodeEqual(tree.items[0], 'eating_cucumbers.feature', 'file', 'test\\resources\\eating_cucumbers.feature');
+      assertNodeEqual(tree.items[1], 'hello_world.feature', 'file', 'test\\resources\\hello_world.feature');
+    });
+  });
+
+  function assertNodeEqual(actualNode, expectedName, expectedType, expectedPath) {
+    assert.equal(actualNode.name, expectedName);
+    assert.equal(actualNode.type, expectedType);
+    assert.equal(actualNode.path, expectedPath);
+  }
+
+});
