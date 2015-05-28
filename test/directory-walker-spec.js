1a105092db6d414de372c7b30abe348f78237e0c
Daniel Pacak <pacak.daniel@gmail.com>
2015-05-28 09:09:24 +0200

    handle paths in a platform independent way

--- test/directory-walker-spec.js
+++ test/directory-walker-spec.js
@@ -1,9 +1,9 @@
 var walker = require('../lib/directory-walker'),
-  assert = require('assert');
+  assert = require('assert'),
+  path = require('path');
 
 describe('directory-walker', function () {
-
-  describe('#walk', function () {
+  describe.only('#walk', function () {
     it('should return directory tree', function () {
       var tree = walker.walk('test/resources', '.');
 
@@ -19,7 +19,7 @@ describe('directory-walker', function () {
   function assertNodeEqual(actualNode, expectedName, expectedType, expectedPath) {
     assert.equal(actualNode.name, expectedName);
     assert.equal(actualNode.type, expectedType);
-    assert.equal(actualNode.path, expectedPath);
+    assert.equal(actualNode.path, path.normalize(expectedPath));
   }
 
 });
