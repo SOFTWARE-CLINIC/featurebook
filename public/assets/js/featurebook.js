4918f6a3b1bfc04d0de517fcf34bdc043d84d966
Daniel Pacak <pacak.daniel@gmail.com>
2015-05-30 16:21:49 +0200

    Display available feature files in the navigation page

--- public/assets/js/featurebook.js
+++ public/assets/js/featurebook.js
@@ -1,3 +1,34 @@
 $(function () {
-  console.log('FeatureBook is ready to rumble!');
+    console.log('FeatureBook is ready to rumble!');
+    createFeaturesTreeElement();
 });
+
+function createFeaturesTreeElement() {
+
+    getFeatures()
+        .done(function (node) {
+            createNodeElement(node, $('<ul/>').appendTo($('#features-tree')));
+        });
+
+    function getFeatures() {
+        return $.ajax('api/rest/feature/tree');
+    }
+
+    function createNodeElement(node, container) {
+        if (node.type === 'folder') {
+            var li = $('<li/>').text(node.name).appendTo(container);
+            $.each(node.items, function (i) {
+                createNodeElement(node.items[i], $('<ul/>').appendTo(li));
+            });
+        } else {
+            var li = $('<li/>').on('click', onFeatureClicked).appendTo(container);
+            $('<a/>').text(node.name).attr('href', '#' + node.path).appendTo(li);
+        }
+
+        function onFeatureClicked() {
+            alert(node.path);
+        }
+    }
+
+
+}
