1cf57b9de549468855860098686c8db0b2d2cfc5
Daniel Pacak <pacak.daniel@gmail.com>
2015-05-31 12:37:24 +0200

    Display selected feature files in the main pane

--- public/assets/js/featurebook.js
+++ public/assets/js/featurebook.js
@@ -1,13 +1,27 @@
 $(function () {
-    console.log('FeatureBook is ready to rumble!');
+    $(window).bind('hashchange', onHashChange);
+    createSummaryElements();
     createFeaturesTreeElement();
 });
+
+function createSummaryElements() {
+
+    getSummary().done(function (summary) {
+        $('.featurebook-title').text(summary.title);
+        $('title').text(summary.title);
+    });
+
+    function getSummary() {
+        return $.ajax('api/rest/summary');
+    }
+}
 
 function createFeaturesTreeElement() {
 
     getFeatures()
         .done(function (node) {
-            createNodeElement(node, $('<ul/>').appendTo($('#features-tree')));
+            createNodeElement(node, $('<ul/>').appendTo($('.featurebook-features-tree')));
+            // TODO Check for hash in the URL; if present mark a corresponding feature as active (CSS class).
         });
 
     function getFeatures() {
@@ -21,14 +35,126 @@ function createFeaturesTreeElement() {
                 createNodeElement(node.items[i], $('<ul/>').appendTo(li));
             });
         } else {
-            var li = $('<li/>').on('click', onFeatureClicked).appendTo(container);
-            $('<a/>').text(node.name).attr('href', '#' + node.path).appendTo(li);
-        }
-
-        function onFeatureClicked() {
-            alert(node.path);
+            var li = $('<li/>').appendTo(container);
+            $('<a/>').text(node.name).attr('href', '#feature/' + encodeURIComponent(node.path)).appendTo(li);
         }
     }
 
+}
 
+function onHashChange() {
+    var path = featurePathFromHash();
+    if (path) {
+        getFeature().done(function (feature) {
+            var container = $('.featurebook-feature');
+
+            container.empty();
+            createFeatureElement(feature, $('.featurebook-feature'));
+        });
+    }
+
+    function getFeature() {
+        return $.ajax('api/rest/feature/parsed/' + path);
+    }
+}
+
+function featurePathFromHash() {
+    var hash = window.location.hash;
+    if (hash && hash.startsWith('#feature/')) {
+        return hash.replace('#feature/', '');
+    }
+    return null;
+}
+
+function createFeatureElement(feature, container) {
+    $('<h3/>').text('Feature: ' + feature.name)
+        .addClass('featurebook-feature-name')
+        .appendTo(container);
+
+    createDescriptionElement(feature.description)
+        .appendTo(container);
+
+    $.each(feature.scenarios, function (i, scenario) {
+        createScenarioElement(scenario)
+            .appendTo(container);
+    });
+
+    $.each(feature.scenario_outlines, function (i, outline) {
+        createScenarioOutlineElement(outline)
+            .appendTo(container);
+    });
+
+    function createDescriptionElement(description) {
+        return $('<div/>')
+            .text(description)
+            .addClass('featurebook-feature-description');
+    }
+
+    function createScenarioElement(scenario) {
+        var scenarioElement = $('<div/>')
+            .addClass('featurebook-scenario');
+
+        $('<h3/>').text('Scenario: ' + scenario.name).appendTo(scenarioElement);
+
+
+        $.each(scenario.steps, function (i, step) {
+            createStepElement(step)
+                .appendTo(scenarioElement);
+        });
+
+        return scenarioElement;
+    }
+
+    function createScenarioOutlineElement(outline) {
+        var outlineElement = $('<div/>')
+            .addClass('featurebook-scenario');
+
+        $('<h3/>').text('Scenario Outline: ' + outline.name).appendTo(outlineElement);
+
+        $.each(outline.steps, function (i, step) {
+            createStepElement(step)
+                .appendTo(outlineElement);
+        });
+
+        createExamplesElement(outline.examples)
+            .appendTo(outlineElement);
+
+        return outlineElement;
+    }
+
+    function createExamplesElement(examples) {
+        var examplesElement = $('<div/>')
+            .addClass('featurebook-examples');
+
+        $('<h3/>').text('Examples').appendTo(examplesElement);
+
+        var table = $('<table/>').appendTo(examplesElement);
+
+        $.each(examples, function (i, row) {
+            var rowElement = $('<tr/>');
+
+            $.each(row, function (i, cell) {
+                $('<td>').text(cell).appendTo(rowElement);
+            });
+
+            rowElement.appendTo(table);
+        });
+
+        return examplesElement;
+    }
+
+    function createStepElement(step) {
+        var stepElement = $('<div/>')
+            .addClass('featurebook-step');
+
+        $('<span/>').addClass('featurebook-keyword').text(step.keyword).appendTo(stepElement);
+        stepElement.append(' ' + escapeHtml(step.name));
+
+        return stepElement;
+    }
+
+}
+
+function escapeHtml(input) {
+    return input.replace(/</g, '&lt;').replace(/>/g, '&gt;')
 }
