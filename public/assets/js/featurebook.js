8ba4ea1ae93e518574dfe8ad594a9b98bcf43792
Daniel Pacak <pacak.daniel@gmail.com>
2015-06-08 18:22:21 +0200

    Migrate to AngularJS

--- public/assets/js/featurebook.js
+++ public/assets/js/featurebook.js
@@ -1,204 +1,87 @@
-$(function () {
-    $(window).bind('hashchange', onHashChange);
-    createSummaryElements();
-    createFeaturesTreeElement();
-});
+'use strict';
 
-function selectFeature() {
-    var path = featurePathFromHash();
-    if (path) {
-        displayFeature(path);
-    }
-}
+(function () {
 
-function createSummaryElements() {
+    angular.module('scFeatureBook', ['ngRoute', 'ngSanitize'])
+        .config(config)
+        .controller('FeatureBookController', FeatureBookController)
+        .controller('FeatureController', FeatureController)
+        .directive('featureTree', featureTreeDirectiveFactory)
+        .directive('feature', featureDirectiveFactory)
+        .filter('encodeURIComponent', encodeURIComponentFilter);
 
-    getSummary().done(function (summary) {
-        var version = summary.version || '',
-            title = summary.title + ' ' + version;
-        $('.featurebook-title').text(title);
-        $('title').text(title);
-    });
+    config.$inject = ['$routeProvider'];
 
-    function getSummary() {
-        return $.ajax('api/rest/summary');
-    }
-}
-
-function createFeaturesTreeElement() {
-
-    getFeatures()
-        .done(function (node) {
-            createNodeElement(node, $('<ul/>').appendTo($('.featurebook-features-tree')));
-            selectFeature();
-        });
-
-    function getFeatures() {
-        return $.ajax('api/rest/feature/tree');
-    }
-
-    function createNodeElement(node, container) {
-        if (node.type === 'folder') {
-            var li = $('<li/>').text(node.name).appendTo(container);
-            $.each(node.items, function (i) {
-                createNodeElement(node.items[i], $('<ul/>').appendTo(li));
+    function config($routeProvider) {
+        $routeProvider
+            .when('/home', {
+                templateUrl: 'home.html'
+            })
+            .when('/feature/:path', {
+                templateUrl: 'feature.html',
+                controller: 'FeatureController',
+                resolve: {
+                    feature: featureResolver
+                }
+            })
+            .otherwise({
+                redirectTo: '/home'
             });
-        } else {
-            var li = $('<li/>').appendTo(container);
-            $('<a/>')
-                .text(node.name)
-                .attr('href', '#feature/' + encodeURIComponent(node.path))
-                .appendTo(li);
+
+        featureResolver.$inject = ['$route', '$http'];
+
+        function featureResolver($route, $http) {
+            return $http.get('api/rest/feature/parsed/' + encodeURIComponent($route.current.params.path));
         }
     }
 
-}
-
-function onHashChange() {
-    var path = featurePathFromHash();
-    if (path) {
-        displayFeature(path);
+    function featureTreeDirectiveFactory() {
+        return {
+            restrict: 'E',
+            template: '<ul><feature ng-repeat="feature in tree"></feature></ul>',
+            replace: true,
+            transclude: true,
+            scope: {
+                tree: '=ngModel'
+            }
+        };
     }
-}
 
-function displayFeature(path) {
-    getFeature()
-        .done(function (feature) {
-            var container = $('.featurebook-feature');
-            container.empty();
-            createFeatureElement(feature, container);
-            markSelectedFeature(path)
-        })
-        .fail(function () {
-            alert('Sorry dude but I have problem displaying `' + decodeURIComponent(path) + '`. Make sure it has a valid syntax.');
+    featureDirectiveFactory.$inject = ['$compile'];
+
+    function featureDirectiveFactory($compile) {
+        return {
+            restrict: 'E',
+            template: '<li><a href="#feature/{{feature.path | encodeURIComponent}}">{{feature.name}}</a></li>',
+            link: function (scope, elm, attrs) {
+                if (scope.feature.items.length > 0) {
+                    elm.append($compile('<feature-tree ng-model="feature.items"></feature-tree>')(scope));
+                }
+            }
+        };
+    }
+
+    FeatureBookController.$inject = ['$scope', '$http', '$window'];
+
+    function FeatureBookController($scope, $http, $window) {
+        $http.get('api/rest/summary').success(function (summary) {
+            $scope.summary = summary;
+            $window.document.title = 'Feature Book: ' + summary.title;
         });
 
-    function getFeature() {
-        return $.ajax('api/rest/feature/parsed/' + path);
-    }
-}
-
-function markSelectedFeature(path) {
-    $('.featurebook-features-tree a').removeClass('active');
-    $('.featurebook-features-tree a[href="#feature/' + path + '"]').addClass('active');
-}
-
-function featurePathFromHash() {
-    var hash = window.location.hash;
-    if (hash && hash.startsWith('#feature/')) {
-        return hash.replace('#feature/', '');
-    }
-    return null;
-}
-
-function createFeatureElement(feature, container) {
-    $('<h3/>').text('Feature: ' + feature.name)
-        .addClass('featurebook-feature-name')
-        .appendTo(container);
-
-    createDescriptionElement(feature.descriptionAsHtml)
-        .appendTo(container);
-
-    if (feature.background) {
-        createBackgroundElement(feature.background)
-            .appendTo(container);
-    }
-
-    $.each(feature.scenarios, function (i, scenario) {
-        createScenarioElement(scenario)
-            .appendTo(container);
-    });
-
-    $.each(feature.scenario_outlines, function (i, outline) {
-        createScenarioOutlineElement(outline)
-            .appendTo(container);
-    });
-
-    function createDescriptionElement(description) {
-        return $('<div/>')
-            .html(description)
-            .addClass('featurebook-feature-description');
-    }
-
-    function createBackgroundElement(background) {
-        var backgroundElement = $('<div/>')
-            .addClass('featurebook-background');
-
-        $('<h3/>').text('Background:').appendTo(backgroundElement);
-
-        $.each(background.steps, function (i, step) {
-            createStepElement(step)
-                .appendTo(backgroundElement);
+        $http.get('api/rest/feature/tree').success(function (featuresTree) {
+            $scope.featuresTree = [featuresTree];
         });
-
-        return backgroundElement;
     }
 
-    function createScenarioElement(scenario) {
-        var scenarioElement = $('<div/>')
-            .addClass('featurebook-scenario');
+    FeatureController.$inject = ['$scope', '$route'];
 
-        $('<h3/>').text('Scenario: ' + scenario.name).appendTo(scenarioElement);
-
-
-        $.each(scenario.steps, function (i, step) {
-            createStepElement(step)
-                .appendTo(scenarioElement);
-        });
-
-        return scenarioElement;
+    function FeatureController($scope, $route) {
+        $scope.feature = $route.current.locals.feature.data;
     }
 
-    function createScenarioOutlineElement(outline) {
-        var outlineElement = $('<div/>')
-            .addClass('featurebook-scenario');
-
-        $('<h3/>').text('Scenario Outline: ' + outline.name).appendTo(outlineElement);
-
-        $.each(outline.steps, function (i, step) {
-            createStepElement(step)
-                .appendTo(outlineElement);
-        });
-
-        createExamplesElement(outline.examples)
-            .appendTo(outlineElement);
-
-        return outlineElement;
+    function encodeURIComponentFilter() {
+        return window.encodeURIComponent;
     }
 
-    function createExamplesElement(examples) {
-        var examplesElement = $('<div/>')
-            .addClass('featurebook-examples');
-
-        $('<h3/>').text('Examples').appendTo(examplesElement);
-
-        var table = $('<table/>').appendTo(examplesElement);
-
-        $.each(examples, function (i, row) {
-            var rowElement = $('<tr/>');
-
-            $.each(row, function (i, cell) {
-                $('<td>').text(cell).appendTo(rowElement);
-            });
-
-            rowElement.appendTo(table);
-        });
-
-        return examplesElement;
-    }
-
-    function createStepElement(step) {
-        var stepElement = $('<div/>')
-            .addClass('featurebook-step');
-
-        $('<span/>').addClass('featurebook-keyword').text(step.keyword).appendTo(stepElement);
-        stepElement.append(' ' + escapeHtml(step.name));
-
-        return stepElement;
-    }
-
-}
-
-function escapeHtml(input) {
-    return input.replace(/</g, '&lt;').replace(/>/g, '&gt;')
-}
+})();
