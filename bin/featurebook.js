c6883802ed7d3c413ea946733807612c339037c6
Daniel Pacak <pacak.daniel@gmail.com>
2015-06-30 23:37:00 +0200

    Refactoring before introducing pluggable builders (pdf, html, docx, etc.)

--- bin/featurebook.js
+++ bin/featurebook.js
@@ -1,39 +1,55 @@
 #!/usr/bin/env node
 
 var pkg = require('../package.json'),
-  program = require('commander'),
-  path = require('path'),
-  featurebook = require('../lib/featurebook');
+    program = require('commander'),
+    path = require('path');
 
 program
-  .version(pkg.version);
+    .version(pkg.version);
 
 program
-  .command('serve [source-dir]')
-  .option('-p, --port <port>', 'port on which to listen to (defaults to 3000)', parseInt)
-  .description('serve source-dir as a system specification')
-  .action(serve);
+    .command('serve [source-dir]')
+    .option('-p, --port <port>', 'port on which to listen to (defaults to 3000)', parseInt)
+    .description('serve source-dir as a system specification')
+    .action(serve);
 
+// FIXME Update command documentation
 program
-  .command('build [source-dir]')
-  .option('-o, --output-dir <output-dir>', 'directory where the static website will be generated (defaults to dist)')
-  .description('build a static website from source-dir')
-  .action(build);
+    .command('build [source-dir]')
+    .option('-f, --format <format>', 'output format')
+    .option('-o, --output-dir <output-dir>', 'directory where the static website will be generated (defaults to dist)')
+    .description('build a static website from source-dir')
+    .action(build);
 
 program.parse(process.argv);
 
 displayHelpIfNoCommandWasProvided();
 
 function serve(sourceDir, options) {
-  featurebook.serve(sourceDir || process.cwd(), options.port || 3000);
+    var serveCommand = require('../lib/command-serve');
+    serveCommand.execute({
+        sourceDir: sourceDir || process.cwd(),
+        port: options.port || 3000
+    });
 }
 
 function build(sourceDir, options) {
-  featurebook.build(sourceDir || process.cwd(), options.outputDir || path.join(process.cwd(), 'dist'));
+    var buildCommand = require('../lib/command-build');
+
+    // TODO Experimenting with "dynamic" builders registration
+    buildCommand.use(require('../lib/builder/builder-html'));
+    buildCommand.use(require('../lib/builder/builder-pdf'));
+    buildCommand.use(require('../lib/builder/builder-docx'));
+
+    buildCommand.execute({
+        sourceDir: sourceDir || process.cwd(),
+        outputDir: options.outputDir || path.join(process.cwd(), 'dist'),
+        format: options.format
+    });
 }
 
 function displayHelpIfNoCommandWasProvided() {
-  if (!process.argv.slice(2).length) {
-    program.help();
-  }
+    if (!process.argv.slice(2).length) {
+        program.help();
+    }
 }
---
+++ features/format/SUMMARY.md
@@ -0,0 +1,8 @@
+## Format
+
+A system specification is a directory containing:
+
+* Gherkin source files
+* The `assets` directory for images and videos that you can refer to from within the Gherkin source files
+* An optional `SUMMARY.md` descriptor
+* An optioanl `featurebook.json` descriptor
