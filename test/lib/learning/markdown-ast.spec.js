92f9bf2d7681272afc24b427cdb8c5eea70a4c80
Daniel Pacak <pacak.daniel@gmail.com>
2015-07-14 16:43:38 +0200

    Experiment with markdown

--- test/lib/learning/markdown-ast.spec.js
+++ test/lib/learning/markdown-ast.spec.js
@@ -1,4 +1,5 @@
 var marked = require('marked'),
+    markdown = require('markdown').markdown,
     chai = require('chai'),
     should = chai.should();
 
@@ -6,6 +7,21 @@ describe('markdown AST', function () {
 
     it('markdown to AST', function () {
         marked('I am using __markdown__.').should.equal('<p>I am using <strong>markdown</strong>.</p>\n');
+    });
+
+    it('xxx', function () {
+        var text = 'This is **my** list:\n\n' +
+            '* item one\n' +
+            '* item two\n\n' +
+            'Next paragraph';
+
+        console.log(markdown.toHTML(text));
+        var tree = markdown.parse(text);
+
+        console.log(tree);
+
+        console.log(markdown.renderJsonML(markdown.toHTMLTree(tree)));
+
     });
 
 });
