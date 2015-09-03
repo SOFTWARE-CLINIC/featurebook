87041cb32aa883dbb28acd073a4091a709005820
Daniel Pacak <pacak.daniel@gmail.com>
2015-09-03 14:32:14 +0200

    feat: Display an error message when there's a problem parsing a Gherkin source file; fix #48

--- public/scripts/feature-viewer.controller.js
+++ public/scripts/feature-viewer.controller.js
@@ -12,10 +12,14 @@
         $activate();
 
         function $activate() {
-            $scope.feature = $route.current.locals.feature;
+            $scope.featureResponse = $route.current.locals.feature;
 
             $scope.edit = function edit() {
                 $location.path('/editor/' + $route.current.params.path);
+            };
+
+            $scope.hasError = function hasError() {
+                return $scope.featureResponse.status !== 'success';
             };
         }
     }
