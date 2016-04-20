de376f53c2621693bcbcfdcde19bae25e654c153
Daniel Pacak <pacak.daniel@gmail.com>
2016-04-20 22:53:25 +0200

    fix: display help when invalid command is provided in cli

--- bin/featurebook.js
+++ bin/featurebook.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
+
 var program = require('commander');
 var featurebook = require('../lib/featurebook-api');
 var path = require('path');
 
 var DEFAULT_SERVE_PORT = 3000;
+var COMMAND_NAMES = ['serve', 'pdf', 'html'];
 
 program
   .version(featurebook.version);
@@ -18,13 +20,13 @@ program
   .command('pdf [source-dir]')
   .option('-o, --output-dir <output-dir>', 'directory where the PDF specification will be generated (defaults to <source-dir>/dist/pdf)')
   .description('build the specification PDF document')
-  .action(buildPdf);
+  .action(pdf);
 
 program
   .command('html [source-dir]')
   .option('-o, --output-dir <output-dir>', 'directory where the HTML specification will be generated (defaults to <source-dir>/dist/html)')
   .description('build the specification HTML document')
-  .action(buildHtml);
+  .action(html);
 
 program.parse(process.argv);
 
@@ -36,20 +38,21 @@ function serve(specDir, options) {
     options.port || DEFAULT_SERVE_PORT);
 }
 
-function buildPdf(specDir, options) {
+function pdf(specDir, options) {
   require('../lib/featurebook-pdf').build(
     specDir || process.cwd(),
     options.outputDir || path.join(process.cwd(), 'dist', 'pdf'));
 }
 
-function buildHtml(specDir, options) {
+function html(specDir, options) {
   require('../lib/featurebook-html').build(
     specDir || process.cwd(),
     options.outputDir || path.join(process.cwd(), 'dist', 'html'));
 }
 
 function displayHelpIfNoCommandWasProvided() {
-  if (!process.argv.slice(2).length) {
+  var args = process.argv.slice(2);
+  if (!args.length || COMMAND_NAMES.indexOf(args[0]) === -1) {
     program.help();
   }
 }
