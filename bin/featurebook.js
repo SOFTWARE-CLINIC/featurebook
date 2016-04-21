10bee6be8ea071f8ba53bbfaa64a1b65778f0cce
Daniel Pacak <pacak.daniel@gmail.com>
2016-04-21 22:18:18 +0200

    refactor: print serve command status

--- bin/featurebook.js
+++ bin/featurebook.js
@@ -11,20 +11,20 @@ program
   .version(featurebook.version);
 
 program
-  .command('serve [source-dir]')
+  .command('serve [spec-dir]')
   .option('-p, --port <port>', 'port on which to listen to (defaults to 3000)', parseInt)
-  .description('serve <source-dir> as a system specification')
+  .description('serve <spec-dir> as a system specification')
   .action(serve);
 
 program
-  .command('pdf [source-dir]')
-  .option('-o, --output-dir <output-dir>', 'directory where the PDF specification will be generated (defaults to <source-dir>/dist/pdf)')
+  .command('pdf [spec-dir]')
+  .option('-o, --output-dir <output-dir>', 'directory where the PDF specification will be generated (defaults to <spec-dir>/dist/pdf)')
   .description('build the specification PDF document')
   .action(pdf);
 
 program
-  .command('html [source-dir]')
-  .option('-o, --output-dir <output-dir>', 'directory where the HTML specification will be generated (defaults to <source-dir>/dist/html)')
+  .command('html [spec-dir]')
+  .option('-o, --output-dir <output-dir>', 'directory where the HTML specification will be generated (defaults to <spec-dir>/dist/html)')
   .description('build the specification HTML document')
   .action(html);
 
@@ -33,21 +33,28 @@ program.parse(process.argv);
 displayHelpIfNoCommandWasProvided();
 
 function serve(specDir, options) {
-  require('../lib/featurebook-serve').execute(
-    specDir || process.cwd(),
-    options.port || DEFAULT_SERVE_PORT);
+  var port = options.port || DEFAULT_SERVE_PORT;
+  var app = require('../lib/featurebook-serve')(specDir || process.cwd(), port, printServeStatus);
+
+  function printServeStatus() {
+    var color = require('bash-color');
+    console.log(color.green('FeatureBook is running on port ' + port));
+    require('dns').lookup(require('os').hostname(), function (err, address) {
+      var shareLink = 'http://' + address + ':' + port;
+      console.log(color.yellow('It is available to all computers in the local network at ' + shareLink));
+      require('opener')(shareLink);
+    });
+  }
 }
 
 function pdf(specDir, options) {
-  require('../lib/featurebook-pdf').build(
-    specDir || process.cwd(),
-    options.outputDir || path.join(process.cwd(), 'dist', 'pdf'));
+  var outputDir = options.outputDir || path.join(process.cwd(), 'dist', 'pdf');
+  require('../lib/featurebook-pdf')(specDir || process.cwd(), outputDir);
 }
 
 function html(specDir, options) {
-  require('../lib/featurebook-html').build(
-    specDir || process.cwd(),
-    options.outputDir || path.join(process.cwd(), 'dist', 'html'));
+  var outputDir = options.outputDir || path.join(process.cwd(), 'dist', 'html');
+  require('../lib/featurebook-html')(specDir || process.cwd(), outputDir);
 }
 
 function displayHelpIfNoCommandWasProvided() {
